db272ba7c6401d0667374173275268374acf2f8f
Martin Nash <martin.j.nash@gmail.com>
2014-08-05 23:28:30 -0700

    Created example app for bug report.

--- BrokenPolygons/ViewController.swift
+++ BrokenPolygons/ViewController.swift
@@ -7,23 +7,38 @@
 //
 
 import Cocoa
+import MapKit
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, MKMapViewDelegate {
                             
+    @IBOutlet weak var theMapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-                                    
+        theMapView.delegate = self
+        
+        let points = [
+            CLLocationCoordinate2DMake(41.000512, -109.050116),
+            CLLocationCoordinate2DMake(41.002371, -102.052066),
+            CLLocationCoordinate2DMake(36.993076, -102.041981),
+            CLLocationCoordinate2DMake(36.99892, -109.045267)
+        ]
+        let polygon = MKPolygon(coordinates: UnsafeMutablePointer(points), count: points.count)
+        let bounding = polygon.boundingMapRect
+        theMapView.setVisibleMapRect(bounding, edgePadding: NSEdgeInsetsMake(20, 20, 20, 20), animated: true)
+        theMapView.addOverlay(polygon)
     }
 
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        if let polygon = overlay as? MKPolygon {
+            let renderer = MKPolygonRenderer(overlay: overlay)
+            renderer.lineWidth = 10
+            renderer.strokeColor = NSColor.greenColor()
+            renderer.fillColor = NSColor.yellowColor()
+            return renderer
         }
-                                    
+        
+        return nil
     }
-
-
 }
 
